d615484aba8b5cabf1ffed1df25a77a27083bf05
LeeChaeEun <hedung09@playwith.co.kr>
2025-05-22 17:26:47 +0900

    배경, 넥서스 이미지 추가 넥서스 기능 구현 시작

---
+++ 241022/Assets/Script/NexusInfo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NexusInfo : MonoBehaviour
+{
+    public int HP;
+    public int Atk;
+
+
+    public void Start()
+    {
+        
+    }
+
+
+
+}
