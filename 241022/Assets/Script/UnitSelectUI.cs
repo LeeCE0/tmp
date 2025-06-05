031219a1362932788a65ae26bcb61f8babef194b
LeeChaeEun <hedung09@playwith.co.kr>
2025-06-05 17:37:12 +0900

    유닛 선택 UI 추가

---
+++ 241022/Assets/Script/UnitSelectUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectUI : MonoBehaviour
+{
+    [SerializeField] GameObject unitStatePanel;
+    [SerializeField] GameObject selectedUnitPanel;
+    [SerializeField] GameObject myUnitPanel;
+}
+ 
